be693523e301b40e465b837186a05d4f788ca284
William Carvalho <90587466+WilliamOCarvalho@users.noreply.github.com>
2022-03-18 02:03:43 -0300

    Reference Types and Values in C#
    Bootcamp Dio GFT - Working with reference types and values ​​in C#

---
+++ DEMOS/Program.cs
@@ -0,0 +1,37 @@
+﻿using static System.Console;
+
+public class program 
+{
+    static void Demo1()
+    {
+        int a = 2;
+        a = Adicionar20(a);
+
+        WriteLine($"O Valor da variavel a é {a}");
+
+    }
+    static int Adicionar20(int x)
+    {
+        return x + 20;
+    }
+
+    static void TrocarNome(Pessoa p1, string nomeNovo)
+    {
+        p1.Nome = nomeNovo;
+    }
+ 
+    public static void Main()
+    {
+      Pessoa p1 = new Pessoa();
+      p1.Nome = "William";
+      p1.Idade = 32;
+      p1.Documento = "1234";
+
+      TrocarNome(p1, "José");
+
+      WriteLine($"O novo nome é :{p1.Nome}");
+
+    }
+
+}
+
